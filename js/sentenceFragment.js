42bce0d2bf16a1f74554664039ae7401c3d1bf76
kpgoodale <kpgoodale@gmail.com>
2018-01-18 13:28:49 -0500

    Added some initial structure and documentation

---
+++ js/sentenceFragment.js
@@ -0,0 +1,15 @@
+var sentence = "I like to move it move it ehhh macarana";
+var sentenceFragment = "move it macarana";
+
+// create a function that compares a sentence to a sentence fragment, and outputs an array of the missing words from the fragment
+let compareFragment = (sentence, fragment) => {
+    // extract the words from sentence and fragment into an array
+
+    // iterate over the sentence to find the missing words.
+
+
+    // return the array
+    return [sentence,fragment];
+}
+
+console.log(compareFragment(sentence, sentenceFragment));
